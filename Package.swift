e721d2dd59e1d494292563f623299a90de6767cd
ps-github-wh-mobile <153748623+ps-github-wh-mobile@users.noreply.github.com>
2023-12-21 17:38:02 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,8 +18,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Pendo",
-            url: "https://github.com/ps-github-wh-mobile/pendosdk/raw/main/ios/pendo-ios-sdk-xcframework.3.0.0.7964.zip",
-            checksum: "ab4f00f4f5be839094d0c05f2d93ae2097efad1c93a57c5e7634c068fe4c3102"
+            url: "https://github.com/ps-github-wh-mobile/pendosdk/raw/main/ios/pendo-ios-sdk-xcframework.3.0.0.7964.zip"
         ),
     ]
 )
